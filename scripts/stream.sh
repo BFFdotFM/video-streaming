8722d4415490b8eadfa23b31df628eeab02e67aa
Ben Ward <3153+BenWard@users.noreply.github.com>
2021-03-17 18:45:27 -0700

    Update stream.sh
    Here's the multi-output code we used in March.

--- scripts/stream.sh
+++ scripts/stream.sh
@@ -1,29 +1,26 @@
 #!/bin/sh
 
-# Script to stream a video file at 720p to Twitch, via FFMPEg.
-#
-# * Bias to higher audio quality
+# Script to stream a video file to Twitch, YouTube and Facebook simultaneously. Presumes pre-encoded using ./encode.sh
 #
 # Usage:
 #
-# ./stream.sh filename.mp4 twitch-stream-id [timestamp]
+# ./stream.sh filename.mp4 [timestamp] [twitchKey] [youtubeKey] [facebookKey]
+#
+# TODO: Source keys from environment variables, only include output streams for RTMPs where var is set
 #
 # Timestamp defaults to 00:00:00 (start from beginning of the file), can be entered if you need to
 # restart a stream recording that crashes mid-way through
 
 # Support starting at a specific timestamp must be of form HH:MM:SS
-if [ -n "$3" ]
+if [ -n "$2" ]
 then
-  ts=$3
+  ts=$2
 else
   ts="0:00:00"
 fi
 
 ffmpeg -ss $ts \
        -re -i $1 \
-# Include audio coded as-per encode.sh, else ffmpeg defaults to transcoding it to MP3
-       -acodec aac -b:a 192k -ar 44100 -ac 2 \
-# Twitch
-#       -f flv rtmp://live.twitch.tv/app/$2 \
-# YouTube Live
-#       -f flv rtmp://a.rtmp.youtube.com/live2/$2
+       -c copy -f flv rtmp://sfo.contribute.live-video.net/app/$3 \
+       -c copy -f flv rtmp://a.rtmp.youtube.com/live2/$4 \
+       -c copy -f flv "rtmps://live-api-s.facebook.com:443/rtmp/$5"
